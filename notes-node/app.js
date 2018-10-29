ad7fb2a33c1ea4d2de850d1df132c2d5716a5d9a
sergeevdms <sergeevdms@gmail.com>
2018-10-29 03:05:56 +0300

    added lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,17 @@ console.log('Strating app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
-var res = notes.addNote(4, 50);
-console.log(res);
+console.log(_.isString(true));
+console.log(_.isString('string'));
+
+var filteredArray = _.uniq(['String', 1, 2, 'String', 2, 6]);
+console.log(filteredArray);
+
+// var res = notes.addNote(4, 50);
+// console.log(res);
 
 // var user = os.userInfo();
 //
