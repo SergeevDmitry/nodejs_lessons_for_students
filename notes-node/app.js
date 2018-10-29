8540976afacf08266c0334fb6b9166599a4f0a7a
sergeevdms <sergeevdms@gmail.com>
2018-10-29 22:52:53 +0300

    added process argv

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,19 +1,17 @@
 console.log('Strating app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes');
 
-console.log(_.isString(true));
-console.log(_.isString('string'));
+var command = process.argv[2];
+console.log(process.argv);
 
-var filteredArray = _.uniq(['String', 1, 2, 'String', 2, 6]);
-console.log(filteredArray);
-
-// var res = notes.addNote(4, 50);
-// console.log(res);
-
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+if (command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else {
+  console.log('Not found');
+}
