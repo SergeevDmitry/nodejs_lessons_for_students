ec8cf57e8341007a1b79f3245bca3c8727e7a28d
sergeevdms <sergeevdms@gmail.com>
2018-10-31 20:54:47 +0300

    refactoring

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
-const request = require('request');
 const yargs = require('yargs');
 
-const GA_KEY = 'AIzaSyDDn9DHeujNua08f9vT2N8lNScEYN3bPSI'
+const geocode = require('./geocode/geocode.js');
+
 const argv = yargs
   .options({
     a: {
@@ -15,21 +15,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-// 1301 lombard street
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${GA_KEY}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
   } else {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitute: ${body.results[0].geometry.location.lng}`);
+    console.log(JSON.stringify(results, undefined, 2));
   }
-})
+});
