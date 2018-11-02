17403df114755a530bb064ae8de9f1ae9d1077ad
sergeevdms <sergeevdms@gmail.com>
2018-11-02 20:43:45 +0300

    added expect in tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,20 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(2);
 
-  if (res !== 4) {
-    throw new Error(`Expected 4, but got ${res}`);
-  }
+  expect(res).toBe(4).toBeA('number');
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(122);
+  expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
 });
