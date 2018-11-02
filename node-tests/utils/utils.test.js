78aa734261886352e333494b9f0bd58c34fc2802
sergeevdms <sergeevdms@gmail.com>
2018-11-02 22:21:39 +0300

    added async tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -8,13 +8,47 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
-it('should square a number', () => {
-  var res = utils.square(2);
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  })
+})
 
-  expect(res).toBe(4).toBeA('number');
+it('should async Square', (done) => {
+  utils.asyncSquare(4, (sq) => {
+    expect(sq).toBe(16).toBeA('number');
+    done();
+  })
+})
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'Philadelphia', age: 25};
+  var res = utils.setName(user, 'Andrew Mead');
+
+  expect(res).toInclude({
+    firstName: 'Andrew',
+    lastName: 'Mead'
+  })
 });
 
-it('should expect some values', () => {
-  // expect(12).toNotBe(122);
-  expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
-});
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(122);
+//   // expect({name: 'Andrew'}).toEqual({name: 'Andrew'});
+//   // expect([2,3,4]).toInclude(2);
+//   // expect([2,3,4]).toExclude(5);
+//   // expect({
+//   //   name: 'Andrew',
+//   //   age: 25,
+//   //   location: 'Philadelphia'
+//   // }).toInclude({
+//   //   age: 25
+//   // })
+//   expect({
+//     name: 'Andrew',
+//     age: 25,
+//     location: 'Philadelphia'
+//   }).toExclude({
+//     age: 22
+//   })
+// });
