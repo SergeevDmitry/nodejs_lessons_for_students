ddb1da476a55060263b0dd21fe5abad332366240
sergeevdms <sergeevdms@gmail.com>
2018-11-07 21:10:50 +0300

    added post request

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {User} = require('./models/user');
+const {Todo} = require('./models/todo');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  // console.log('receiving data...');
+  // console.log('body is ',req.body);
+  // res.send(req.body);
+
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.status(201).send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
